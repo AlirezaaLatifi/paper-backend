99498f5bc56323b8c1e10adb8ee274f90f055e22
alirezaa-latifi <l.alirezalatifi@gmail.com>
2023-05-02 19:42:39 +0430

    add some origins to whiteList

--- config/whitList.js
+++ config/whitList.js
@@ -1,6 +1,6 @@
 const whiteList = [
-  "https://www.mywebsite1.com",
-  "https://www.mywebsite2.com",
+  "https://alirezaa-latifi.github.io/paper-frontend",
   "http://localhost:5173",
+  "http://localhost:4173",
 ];
 module.exports = whiteList;
